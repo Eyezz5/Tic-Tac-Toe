5f55b12ad76f31f076778e253aa9ea2c677e0fed
ihateuu <replacement506@gmail.com>
2021-07-27 20:15:42 +0300

    Created Cell Tiles

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           Text("Tic Tac Toe")
-            .font(.title)
-            .fontWeight(.bold)
+            Text("Tic Tac Toe")
+                .font(.title)
+                .fontWeight(.bold)
+            LazyVGrid(columns: Array(repeating : GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
+                ForEach(0..<9) { index in
+                    Color.blue
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .cornerRadius(30)
+                }
+            })
         }
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
